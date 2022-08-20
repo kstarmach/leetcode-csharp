1b87457ec3155aca4d83eb4e17d8c1514e610237
Kamil Starmach <kamil.starmach@hotmail.com>
2022-08-20 16:32:12 +0200

    update

--- LeetCode/Problems/Problem_27_RemoveElement.cs
+++ LeetCode/Problems/Problem_27_RemoveElement.cs
@@ -4,16 +4,30 @@ public class Problem_27_RemoveElement
 {
     public int RemoveElement(int[] nums, int val)
     {
-        int i = 0;
-        foreach (var item in nums)
+
+        // int i = 0;
+        // foreach (var item in nums)
+        // {
+        //     if (item != val)
+        //     {
+        //         nums[i] = item;
+        //         i++;
+        //     }
+        // }
+        // return i;
+
+        int position = 0;
+
+        for (int i = 0; i < nums.Length; i++)
         {
-            if (item != val)
+            if (nums[i] != val)
             {
-                nums[i] = item;
-                i++;
+                nums[position++] = nums[i];
             }
+
         }
-        return i;
+
+        return position;
     }
 
 }
