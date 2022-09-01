9e4ef257d25dc49268c1305a6c84dbb2088d6eea
Kamil Starmach <kamil.starmach@hotmail.com>
2022-09-01 13:11:41 +0200

    142. Linked List Cycle II

---
+++ LeetCode.Test/Tests/Test_142_LinkedListCycleII.cs
@@ -0,0 +1,15 @@
+namespace LeetCode.Test;
+
+[TestClass]
+public class Test_142_LinkedListCycleII
+{
+    // [TestMethod]
+    // public void DetectCycle_Test1()
+    // {
+    //     var solution = new Problem_142_LinkedListCycleII();
+
+    //     var input = TestHelper.GenerateList(new int[] { 3, 2, 0, -4 });
+    //     var result = solution.DetectCycle(input);
+    //     AssertHelper.AssertLinkList(new int[] { 2, 0, -4 }, result);
+    // }
+}
